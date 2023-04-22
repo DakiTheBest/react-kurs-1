339fa135aa283c525a23c8d00c3c6abc0a676869
dark0k_ <knezevicdarko899@gmail.com>
2023-04-22 20:08:59 +0200

    object submit

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,11 +1,21 @@
 import React from "react";
-import "./NewExpense.css"
+import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = () => {
-  return <div className="new-expense">
-    <ExpenseForm />
-  </div>
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    console.log(expenseData);
+  };
+
+  return (
+    <div className="new-expense">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
 };
 
 export default NewExpense;
